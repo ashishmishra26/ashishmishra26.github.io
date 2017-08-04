501bcd7a5d9c1a617f63f74de8b1bb89d65beb17
Ashish Mishra <b.s.ashish26@gmail.com>
2017-08-04 11:06:12 +0530

    unit testing

---
+++ spec/simonspec.js
@@ -0,0 +1,339 @@
+describe("set of tests", function () {
+  beforeAll(function () {
+    var temp = '<div id="mssg"><p></p></div><div id="outer-circle"> <button type="button" id="button-1" onclick="addPlayerMove("button-1")"></button> <button type="button" id="button-2" onclick="addPlayerMove("button-2")"></button> <button type="button" id="button-3" onclick="addPlayerMove("button-3")"></button> <button type="button" id="button-4" onclick="addPlayerMove("button-4")"></button> <div id="inner-circle"> <div id="inner-circle-content"> <h2 id="text-font">Simon</h2> <input type="text" id="strict" readonly> <br><div><input type="text" name="counter" size="5px" placeholder="" id="count"> <font id="label1">COUNT</font> </div><d> <button type="button" id="button-5" onclick="start()" value="start"></button> <font id="label2">START</font> </d iv> <div><button type="button" id="button-6" onclick="setStrict()"></button> <font id="label3">STRICT</font> </div><div id="switch-container"> <font id="label4">OFF</font> <label class="switch"> <input type="checkbox" onclick="toggle()" id="check"> <span class="slider"></span> </label> <font id="label5">ON</font> </div></div></div></div></div><audio id="Green"> <source src="assets/audio/Green.mp3" </source> <source src="assets/audio/Green.ogg"> </audio> <audio id="Red"> <source src="assets/audio/Red.mp3" </source> <source src="assets/audio/Red.ogg"> </audio> <audio id="Yellow"> <source src="assets/audio/Yellow.mp3" </source> <source src="assets/audio/Yellow.ogg"> </audio> <audio id="Blue"> <source src="assets/audio/Blue.mp3" </source> <source src="assets/audio/Blue.ogg"> </audio> <audio id="Wrong"> <source src="assets/audio/Wrong.ogg"> <source src="assets/audio/Wrong.ogg"> </audio>';
+    document.body.insertAdjacentHTML("afterbegin", temp);
+  });
+  
+  describe("reset if function test", function () {
+    it("should call setstrict", function () {
+       game.strict = true;
+      var spy = GameObject;
+      spyOn(spy, 'setStrict');
+      GameObject.reset();
+      expect(spy.setStrict).toHaveBeenCalled();
+    });
+  });
+
+  describe("test game object", function () {
+    it("test", function () {
+      expect(game.stage).toBe(0);
+      expect(game.strict).toBe(false);
+    });
+  });
+
+  describe("setstrict function test", function () {
+    it("test", function () {
+      document.getElementById("check").checked = true;
+      GameObject.setStrict();
+      game.srict = false;
+      expect(document.getElementById("strict").style.backgroundColor).toBe("red");
+    });
+  });
+
+  describe("check setstrict function test two", function () {
+   it("should be black", function () {
+      document.getElementById("check").checked = false;
+      GameObject.setStrict();
+      expect(document.getElementById("strict").style.backgroundColor).toBe("black");
+    });
+  });
+
+  describe("displayCount test", function () {
+    it("display count test", function () {
+      game.stage = '19';
+      GameObject.displayCount();
+      expect(document.getElementById("count").value).toBe(game.stage);
+    });
+  });
+
+  describe("check setstrict function test", function () {
+    it("test", function () {
+      document.getElementById("check").checked = true;
+      GameObject.setStrict();
+      game.srict = true;
+      expect(document.getElementById("strict").style.backgroundColor).toBe("black");
+    });
+  });
+
+  describe("showMove test", function () {
+    var id = "button-1";
+    it("should change opacity", function () {
+      document.getElementById("check").checked = true;
+      GameObject.showMove(id);
+      expect(document.getElementById(id).style.opacity).toBe('0.6');
+    });
+    it("should change opacity", function () {
+      document.getElementById("check").checked =false;
+      GameObject.showMove(id);
+      expect(GameObject.showMove()).toBe(56);
+    });
+  });
+
+  describe("addPlayermove tese", function () {
+    beforeEach(function () {
+      document.getElementById("check").checked = false;
+      GameObject.addPlayerMove();
+    });
+    it("should return something", function () {
+      expect(GameObject.addPlayerMove()).toBe(5);
+    });
+  });
+  
+  describe("audio test", function () {
+    it("audio one", function () {
+      GameObject.playAudio("button-2");
+    });
+    it("audio two", function () {
+      GameObject.playAudio("button-3");
+    });
+    it("audio four", function () {
+      GameObject.playAudio("button-4");
+    });
+  });
+
+  describe("start function test", function () {
+    it("should call addcount", function () {
+      var spy = GameObject;
+      spyOn(spy,'addCount');
+      GameObject.start();
+      expect(spy.addCount).toHaveBeenCalled();
+    });
+  });
+  
+  describe("Add count test", function () {
+    it("should call displaycount", function () {
+      var spy = GameObject;
+      spyOn(spy,'displayCount');
+      GameObject.addCount();
+      expect(spy.displayCount).toHaveBeenCalled();
+    });
+    it("should call generate sequence", function () {
+      var spy = GameObject;
+      spyOn(spy, 'generateSequence');
+      GameObject.addCount();
+      expect(spy.generateSequence).toHaveBeenCalled();
+    });
+  });
+  
+  describe("window onload test",function(){
+   it("buttonDIsable shoub be called",function(){
+    var spy=GameObject;
+    spyOn(spy,'buttonDisable');
+    window.onload();
+    expect(spy.buttonDisable).toHaveBeenCalled();
+   });
+  });
+  
+  describe("generate sequence function test", function () {
+    it("should call showsequence", function () {
+      document.getElementById("check").checked = true;
+      var spy = GameObject;
+      spyOn(spy, 'showSequence');
+      GameObject.generateSequence();
+      expect(spy.showSequence).toHaveBeenCalled();
+    });
+  });
+  
+  describe("show sequence test", function () {
+    it("spy testing", function () {
+      document.getElementById("check").checked =true;
+      game.stage = 1;
+      var spy = GameObject;
+      spyOn(spy, 'buttonDisable');
+      spy.showSequence();
+      expect(spy.buttonDisable).toHaveBeenCalled();
+      expect(document.getElementById("count").value).toBe("0" + game.stage);
+    });
+
+    it("spy testing", function () {
+      document.getElementById("check").checked =true;
+      game.stage = '11';
+      GameObject.showSequence();
+      expect(document.getElementById("count").value).toBe(game.stage);
+    });
+    
+    it("spy testing", function () {
+      document.getElementById("check").checked =true;
+      game.stage = 1;
+      game.currentSequence = ["button-1"];
+      jasmine.clock().install();
+      var spy = GameObject;
+      spyOn(spy, 'showMove');
+      spy.showSequence();
+      jasmine.clock().tick(801);
+      expect(spy.showMove).toHaveBeenCalled();
+      jasmine.clock().uninstall();
+    });
+    
+    it("spy testing", function () {
+      jasmine.clock().install();
+      var spy = GameObject;
+      spyOn(spy, 'buttonEnable');
+      spy.showSequence();
+      jasmine.clock().tick(game.stage * 1000);
+      jasmine.clock().uninstall();
+    });
+    
+    it("spy testing", function () {
+      document.getElementById("check").checked = true; 
+      game.stage = 0;
+      game.currentSequence = [];
+      GameObject.showSequence();
+      expect(GameObject.showSequence()).toBe(22);
+    });
+    
+    it("",function(){
+      document.getElementById("check").checked = false;
+      GameObject.showSequence();
+      expect(GameObject.showSequence()).toBe(23);
+    });
+  });
+
+  describe("show move test", function () {
+    var id = "button-1";
+    it("", function () {
+      jasmine.clock().install();
+      document.getElementById("check").checked = true;
+      GameObject.showMove(id);
+      expect(document.getElementById(id).style.opacity).toBe("0.6");
+      jasmine.clock().tick(301);
+      expect(document.getElementById(id).style.opacity).toBe("1");
+      jasmine.clock().uninstall();
+    });
+  });
+
+  describe("wrong blink test", function () {
+    it("should set value", function () {
+      game.stage = 0;
+      jasmine.clock().install();
+      GameObject.wrongBlink();
+      jasmine.clock().tick(201);
+      expect(document.getElementById("count").value).toBe("!" + " " + "!");
+      jasmine.clock().uninstall();
+    });
+    it("", function () {
+      game.stage = 1;
+      jasmine.clock().install();
+      GameObject.wrongBlink();
+      jasmine.clock().tick(201);
+      var spy = GameObject;
+      spyOn(spy,'displayCount');
+      //expect(spy.displayCount).toHaveBeenCalled();
+      jasmine.clock().uninstall();
+    });
+
+  });
+  
+  describe("playerTurn test", function () {
+    beforeEach(function () {
+      game.strict = true;
+      document.getElementById("check").checked = true;
+    });
+    it("testing", function () {
+      var spy = GameObject;
+      spyOn(spy, 'playerTurn');
+      spy.addPlayerMove();
+      expect(spy.playerTurn).toHaveBeenCalled();
+    });
+  });
+
+  describe("ToggleTest", function () {
+    it("should", function () {
+      document.getElementById("check").checked = false;
+      var spy = GameObject;
+      spyOn(spy, 'reset');
+      GameObject.toggle();
+      expect(spy.reset).toHaveBeenCalled();
+    });
+    it("should", function () {
+      document.getElementById("check").checked = true;
+      GameObject.toggle();
+      expect(document.getElementById("count").value).toBe("--");
+    });
+
+  });
+
+  describe("buttonEnable", function () {
+    it("should return 5", function () {
+      expect(GameObject.buttonEnable()).toBe(5);
+    });
+  });
+
+
+  describe("player turn", function () {
+    beforeEach(function () {
+      var length1 = game.playerMoves.length;
+      var length2 = game.currentSequence.length;
+      game.playerMoves = ["button-1"];
+      game.currentSequence = ["button-1"];
+    });
+    it("testing", function () {
+      game.playerMoves = ["button-1"];
+      game.currentSequence = ["button-1,button-2"];
+      document.getElementById("check").checked = true;
+      game.strict = true;
+      var spy = GameObject;
+      spyOn(spy, 'buttonDisable');
+      spy.playerTurn();
+      expect(spy.buttonDisable).toHaveBeenCalled();
+    });
+
+    it("testing", function () {
+      game.playerMoves = ["button-1"];
+      game.currentSequence = ["button-1,button-2"];
+      document.getElementById("check").checked = true;
+      game.strict = false;
+      var spy = GameObject;
+      spyOn(spy, 'buttonDisable');
+      spy.playerTurn();
+      expect(spy.buttonDisable).toHaveBeenCalled();
+    });
+
+    it("testing", function () {
+      document.getElementById("check").checked = false;
+      game.strict = false;
+      var spy = GameObject;
+      spyOn(spy, 'addCount');
+      spy.playerTurn();
+      expect(spy.addCount).toHaveBeenCalled();
+    });
+
+    it("testing", function () {
+      document.getElementById("check").checked = false;
+      game.strict = false;
+      game.stage = 20;
+      jasmine.clock().install();
+      
+      GameObject.playerTurn();
+      expect(document.getElementById("mssg").innerHTML).toEqual("YOU WON");
+      jasmine.clock().tick(2001);
+      expect(document.getElementById("mssg").innerHTML).toBe("");
+     
+    });
+  });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+/*describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+  
+
+    expect(game.stage).toBe(0);
+  });
+});
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+
+  });
+});*/
--- src/simon.js
+++ src/simon.js
@@ -1,213 +1,212 @@
-var game = { //object which contain properties of game.
+
+var game= { //object which contain properties of game.
     stage: 0, //current stage of game.
-    possible: ['button-1', 'button-2', 'button-3', 'button-4'], //array for possible value for random generator.
+    possible: ["button-1","button-2","button-3","button-4"], //array for possible value for random generator.
     currentSequence: [], //array for current sequence of game
     playerMoves: [], //array for storing player moves
     strict: false, //variable for strict mode state
     timeout: NaN,
+    test:0,
 };
+var GameObject={};
 
-function reset() { //function for reseting the game.                  
+GameObject.reset=function (){ //function for reseting the game.                  
     if (game.strict === true) {
-        setStrict();
+        GameObject.setStrict();
     }
     clearTimeout(game.timeout);
     document.getElementById("count").value = "";
-    buttonDisable();
+    GameObject.buttonDisable();
     game.currentSequence = [];
     game.playerMove = [];
     game.strict = false;
     game.stage = 0;
-}
-
-window.onload = function () { //to disable buttons when game starts.
-    buttonDisable();
 };
 
-function checkOnOff() { //to check status of toggle button.
-    return (document.getElementById('check').checked);
-}
+window.onload = function () { //to disable buttons when game starts.
+    GameObject.buttonDisable();
+};
 
-function setStrict() { //to set and reset strict button.
-    if (checkOnOff() === true) {
+GameObject.checkOnOff=function () { //to check status of toggle button.
+    return document.getElementById("check").checked;
+};
+
+GameObject.setStrict=function() { //to set and reset strict button.
+    if (GameObject.checkOnOff() === true) {
         if (game.strict === false) {
-            document.getElementById('strict').style.backgroundColor = "red";
+            document.getElementById("strict").style.backgroundColor = "red";
             game.strict = true;
         } else {
-            document.getElementById('strict').style.backgroundColor = "black";
+            document.getElementById("strict").style.backgroundColor = "black";
             game.strict = false;
         }
         return game.strict;
     } else {
-        document.getElementById('strict').style.backgroundColor = "black";
+        document.getElementById("strict").style.backgroundColor = "black";
     }
-}
+};
+GameObject.start=function (){ //initial function, called when start button is clicked.
+    game.stage = 0;
+    game.currentSequence = [];
+    game.playerMoves = [];
+    GameObject.addCount(); //calls addcount function when game is on.
+};
 
-function start() { //initial function, called when start button is clicked.
-    if (checkOnOff() === true) {
-        game.stage = 0;
-        game.currentSequence = [];
-        game.playerMoves = [];
-        addCount(); //calls addcount function when game is on.
-    } else {
-        return; //if game is off return.
-    }
-}
-
-function addCount() { //to increase the count of stages.
+GameObject.addCount=function () { //to increase the count of stages.
     game.stage++;
-    displayCount();
-    generateSequence(); //call to generate sequence of random button.
-}
+    GameObject.displayCount();
+    GameObject.generateSequence(); //call to generate sequence of random button.
+};
 
-function displayCount() {
+GameObject.displayCount=function() {
     if (game.stage.toString().length == 1) {
-        document.getElementById('count').value = "0" + game.stage;
+        document.getElementById("count").value = "0" + game.stage;
     } else {
-        document.getElementById('count').value = game.stage;
+        document.getElementById("count").value = game.stage;
     }
-}
+};
 
-function generateSequence() { //for generating sequence
-    if (checkOnOff() === true) {
+GameObject.generateSequence=function() { //for generating sequence
+    if (GameObject.checkOnOff() === true) {
         game.currentSequence.push(game.possible[(Math.floor(Math.random() * 4))]); //use of Math.random to generate random number.
-        showSequence(); //function call to show sequence to user by flashing buttons.
+        GameObject.showSequence(); //function call to show sequence to user by flashing buttons.
     }
-}
+};
 
-function showSequence() { //function to show the sequence by flashing the buttons.
-    var i = 0,
-        moves;
-    if (checkOnOff() === true) {
-        buttonDisable(); //disable buttons while showing sequence
+GameObject.showSequence=function(){ //function to show the sequence by flashing the buttons.
+    var i = 0,moves;
+    if (GameObject.checkOnOff() === true) {
+        GameObject.buttonDisable(); //disable buttons while showing sequence
         document.getElementById("button-5").setAttribute("disabled", "disabled");
         if (game.stage.toString().length == 1) {
-            document.getElementById('count').value = "0" + game.stage;
+            document.getElementById("count").value = "0" + game.stage;
         } else {
-            document.getElementById('count').value = game.stage;
+            document.getElementById("count").value = game.stage;
         }
         if (game.currentSequence.length != 0) {
             moves = setInterval(function () { //set an interval to control sequence.
-                showMove(game.currentSequence[i]); //function call to flash each buttons.
+                GameObject.showMove(game.currentSequence[i]); //function call to flash each buttons.
                 i = i + 1;
                 if (i >= game.currentSequence.length) {
                     clearInterval(moves); //if all button are shown to user,clear the interval.
                 }
-            }, 800);
+            },800);
             setTimeout(function () {
-                buttonEnable(); //enable the buttons once again
+                GameObject.buttonEnable(); //enable the buttons once again
                 document.getElementById("button-5").removeAttribute("disabled");
             }, (game.stage * 1000));
             game.playerMoves = [];
+        }else{
+            return 22;
         }
+    }else{
+        return 23;
     }
-}
+};
 
-function showMove(id) { //for showing perticular button and audio play.
-    if (checkOnOff() == true) {
+GameObject.showMove = function(id){ //for showing perticular button and audio play.
+    if (GameObject.checkOnOff() == true) {
         document.getElementById(id).style.opacity = "0.6";
-        playAudio(id);
+        GameObject.playAudio(id);
         setTimeout(function () {
             document.getElementById(id).style.opacity = "1";
         }, 300);
+    }else{
+        return 56;
     }
-}
+};
 
-function addPlayerMove(id) { //called when user clicks button as a move.
-    if (checkOnOff() === true) {
+GameObject.addPlayerMove=function(id) { //called when user clicks button as a move.
+    if (GameObject.checkOnOff() === true) {
         game.playerMoves.push(id); //add the button to playerMoves array.
-        playAudio(id); //play audio of perticular button.
-        playerTurn(id); //call playerTurn with button id.
-    } else {
-        return;
+        GameObject.playAudio(id); //play audio of perticular button.
+        GameObject.playerTurn(id); //call playerTurn with button id.
+    }else {
+        return 5;
     }
-}
+};
 
-function wrongBlink() { //to blink the wrong signal when user clicks wrong button in sequence.
+GameObject.wrongBlink=function() { //to blink the wrong signal when user clicks wrong button in sequence.
     setTimeout(function () {
         document.getElementById("count").value = "!" + " " + "!";
     }, 0);
     setTimeout(function () {
         if (game.stage != 0) {
-            displayCount();
+            GameObject.displayCount();
         } else {
             document.getElementById("count").value = "!" + " " + "!";
         }
     }, 200);
-}
+};
 
-function playerTurn(input) {
+GameObject.playerTurn=function() {
     var length1 = game.playerMoves.length;
     var length2 = game.currentSequence.length;
     if (game.playerMoves[length1 - 1] !== game.currentSequence[length1 - 1]) { //check if user input is right.
         if (game.strict === true) { //check if strict mode is on
-            buttonDisable();
-            playAudio('wrong');
-            wrongBlink();
+            GameObject.buttonDisable();
+            GameObject.playAudio("wrong");
+            GameObject.wrongBlink();
             game.currentSequence = [];
             game.playerMoves = [];
-            start();
-        } else { //if strict mode is off 
-            buttonDisable();
-            playAudio('wrong');
-            wrongBlink();
-            showSequence(); //show the sequence again.
+            GameObject.start();
+        }else { //if strict mode is off 
+            GameObject.buttonDisable();
+            GameObject.playAudio("wrong");
+            GameObject.wrongBlink();
+            GameObject.showSequence(); //show the sequence again.
         }
-    } else {
+    } else{
         if (length1 == length2) { //check if user has clicked all buttons of sequence correctly.
             if (game.stage === 20) { //check if current stage of game is 20 
                 document.getElementById("mssg").innerHTML = "YOU WON";
-                reset();
+                GameObject.reset();
                 setTimeout(function () {
                     document.getElementById("mssg").innerHTML = "";
                 }, 2000);
                 document.getElementById("check").checked = false;
             } else { //if final stage is not cleared.
-                addCount(); //call addCount function to increase the level.
+                GameObject.addCount(); //call addCount function to increase the level.
             }
         }
     }
-}
+};
 
-function playAudio(id) //for playing sounds.
-{
-    var x;
-    if (id == 'button-1') {
-        x = document.getElementById("Red");
-        x.play();
-    } else if (id == 'button-2') {
-        x = document.getElementById("Blue");
-        x.play();
-    } else if (id == 'button-3') {
-        x = document.getElementById("Green");
-        x.play();
-    } else if (id == 'button-4') {
-        x = document.getElementById("Yellow");
-        x.play();
-    } else if (id == 'wrong') {
-        x = document.getElementById("Wrong");
-        x.play();
+GameObject.playAudio=function(id){
+    if (id == "button-1") {
+        document.getElementById("Red").play();
+    } else if (id == "button-2") {
+        document.getElementById("Blue").play();
+    } else if (id == "button-3") {
+        document.getElementById("Green").play();
+    } else if (id == "button-4") {
+        document.getElementById("Yellow").play();
+    } else if (id == "wrong") {
+        document.getElementById("Wrong").play();
     }
-}
+};
 
-function toggle() { //change toggle switch
-    if (checkOnOff() === false) {
-        reset();
+GameObject.toggle=function() { //change toggle switch
+    if (GameObject.checkOnOff() === false) {
+        GameObject.reset();
     } else {
         document.getElementById("count").value = "--";
     }
-}
+};
 
-function buttonDisable() { //To disable button.
-    document.getElementById("button-1").setAttribute("disabled", "disabled");
-    document.getElementById("button-2").setAttribute("disabled", "disabled");
-    document.getElementById("button-3").setAttribute("disabled", "disabled");
-    document.getElementById("button-4").setAttribute("disabled", "disabled");
-}
+GameObject.buttonDisable=function() { //To disable button.
+    document.getElementById("button-1").disabled=true;
+    document.getElementById("button-2").disabled=true;
+    document.getElementById("button-3").disabled=true;
+    document.getElementById("button-4").disabled=true;
+   
+};
 
-function buttonEnable() { //to enable buttons.
-    document.getElementById("button-1").removeAttribute("disabled");
-    document.getElementById("button-2").removeAttribute("disabled");
-    document.getElementById("button-3").removeAttribute("disabled");
-    document.getElementById("button-4").removeAttribute("disabled");
-}
+GameObject.buttonEnable=function(){ //to enable buttons.
+    document.getElementById("button-1").disabled=false;
+    document.getElementById("button-2").disabled=false;
+    document.getElementById("button-3").disabled=false;
+    document.getElementById("button-4").disabled=false;
+    return 5;
+};
+
